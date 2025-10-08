ee5c0bd0e3c4f610dcf84010adbc8cfbcda99f5b
juliennepanes <57930589+iyabc@users.noreply.github.com>
2025-10-08 19:30:22 +0800

    fix deployment errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,8 @@ module.exports = {
         './app/**/*.{js,ts,jsx,tsx}',
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
+        './sections/**/*.{js,ts,jsx,tsx}',
+        './ui/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         extend: {},
