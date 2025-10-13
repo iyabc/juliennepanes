1e3c3348ceac8c303ddbefd38080f93e40f67160
juliennepanes <57930589+iyabc@users.noreply.github.com>
2025-10-13 18:43:49 +0800

    add tech

--- app/sections/TechStack.tsx
+++ app/sections/TechStack.tsx
@@ -10,6 +10,11 @@ type TechStackListType = {
 
 const TechStackList: TechStackListType[] = [
     {
+        name: "Java",
+        description: "Proficient in Java programming for building scalable applications.",
+        icon: "/logos/java.svg"
+    },
+    {
         name: "Java Spring",
         description: "Experienced in building robust backend services using Java Spring framework.",
         icon: "/logos/spring.svg"
@@ -40,14 +45,29 @@ const TechStackList: TechStackListType[] = [
         icon: "/logos/mysql.svg"
     },
     {
+        name: "Figma",
+        description: "Experienced in UI/UX design and prototyping using Figma.",
+        icon: "/logos/figma.svg"
+    },
+    {
         name: "Git",
         description: "Proficient in version control and collaboration using Git.",
         icon: "/logos/git.svg"
     },
     {
-        name: "Figma",
-        description: "Experienced in UI/UX design and prototyping using Figma.",
-        icon: "/logos/figma.svg"
+        name: "Jira",
+        description: "Experienced in project management and issue tracking using Jira.",
+        icon: "/logos/jira.svg"
+    },
+    {
+        name: "Redmine",
+        description: "Skilled in project tracking and management using Redmine.",
+        icon: "/logos/redmine.svg"
+    },
+    {
+        name: "AWS",
+        description: "Proficient in deploying and managing applications on Amazon Web Services (AWS).",
+        icon: "/logos/aws.svg"
     }
 ];
 
@@ -56,8 +76,16 @@ const TechStack = () => {
         <Section id="TechStack" title="Tech Stack" aria-label="Tech Stack Section" className='flex-1'>
             <ul className='flex flex-wrap gap-4 justify-center items-center mx-auto'>
                 {TechStackList.map((tech, index) => (
-                    <li key={index} className='relative flex w-8 h-8 md:w-14 md:h-14 justify-between items-center' >
+                    <li key={index} className='relative group flex w-8 h-8 md:w-14 md:h-14 justify-between items-center' >
                         <Image src={tech.icon} alt={`${tech.name} logo`} className='absolute' draggable={false} fill />
+                        <div
+                            className="absolute bottom-full left-1/2 
+                       transform -translate-x-1/2 mb-2 
+                       w-max px-2 py-1 text-sm text-white
+                       bg-background rounded shadow-lg 
+                       opacity-0 group-hover:opacity-100">
+                            <strong>{tech.name}</strong>
+                        </div>
                     </li>
                 ))}
             </ul>
